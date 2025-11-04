6be9232c501c5d02397eea6c971236d49ad2dfdd
Vinne1217 <vincent.korpela@gmail.com>
2025-11-04 13:18:15 +0100

    Disable optimizeCss to fix critters module error

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    optimizeCss: true
-  },
   redirects: async () => [
     { source: '/webshop.html', destination: '/collection', permanent: true },
     { source: '/exhibitionsmessenmaumlssor.html', destination: '/events', permanent: true }
