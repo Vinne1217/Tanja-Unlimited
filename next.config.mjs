43d09bbae72171cd79b6486403b76933ed7bb76f
Vinne1217 <vincent.korpela@gmail.com>
2025-11-04 16:34:38 +0100

    Remove experimental.dynamicIO config (not available in stable Next.js)

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // Disable static optimization for all pages to support useSearchParams
-  experimental: {
-    dynamicIO: true
-  },
   redirects: async () => [
     { source: '/webshop.html', destination: '/collection', permanent: true },
     { source: '/exhibitionsmessenmaumlssor.html', destination: '/events', permanent: true }
